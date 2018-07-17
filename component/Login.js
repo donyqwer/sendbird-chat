2c77540489d784ff0982fd28c2a4d2c35a31fc6e
dony <dony@wobb.co>
2018-07-17 11:00:30 +0800

    Add login form

---
+++ component/Login.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
+import StatusBar from './common/StatusBar';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View>
+        <StatusBar color={"rgba(0, 0, 0, 1)"} /> 
+        <View style={styles.containerStyle}>
+          <View style={styles.containerStyle}>
+            <FormLabel>User ID</FormLabel>
+            <FormInput/>
+          </View>
+          <View style={styles.containerStyle}>
+            <FormLabel>Nickname</FormLabel>
+            <FormInput/>
+          </View>
+          <View style={styles.containerStyle}>
+              <Button
+                buttonStyle={{backgroundColor: '#2096f3'}}
+                title='Connect' 
+              />
+          </View>
+          <View style={styles.containerStyle}>
+              <FormValidationMessage>Error message</FormValidationMessage>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }
+});
---
+++ component/common/StatusBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Expo from 'expo';
+import {  View } from 'react-native';
+import PropTypes from 'prop-types'
+
+export default class StatusBar extends Component {
+  render() {
+    return (
+      <View style={
+        {
+          backgroundColor: this.props.color,
+          height: Expo.Constants.statusBarHeight, 
+          width: null,
+        }
+      }/>
+    );
+  }
+}
+
+StatusBar.propTypes = {
+  color: PropTypes.string.isRequired,
+}
